a4a37c1037796a72b839743de3e076d999ae4c94
mysterymarc <marcin_mac@poczta.onet.pl>
2019-05-11 20:52:51 +0200

    Remove unncessary files. Add cofiguration for Spring Boot

--- src/main/java/pl/mm/sportmetrics/SportmetricsApplication.java
+++ src/main/java/pl/mm/sportmetrics/SportmetricsApplication.java
@@ -1,7 +1,9 @@
 package pl.mm.sportmetrics;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SportmetricsApplication {
 
     public static void main(String[] args) {
