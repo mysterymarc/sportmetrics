cc85822cfba48d231faa2bfdbc0d05a0d95a0632
mysterymarc <marcin_mac@poczta.onet.pl>
2019-05-19 18:14:05 +0200

    test jenkins commit

--- src/main/java/pl/mm/sportmetrics/SportmetricsApplication.java
+++ src/main/java/pl/mm/sportmetrics/SportmetricsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SportmetricsApplication {
-
+//test jenkins
     public static void main(String[] args) {
         SpringApplication.run(SportmetricsApplication.class, args);
     }
