01f01acb3b8ae1fd8b081c87e60f9d46f7c12029
mysterymarc <marcin_mac@poczta.onet.pl>
2019-05-21 20:53:55 +0200

    Change configuration to build war due to issues with blank jsp from jar

--- src/main/java/pl/mm/sportmetrics/SportmetricsApplication.java
+++ src/main/java/pl/mm/sportmetrics/SportmetricsApplication.java
@@ -2,9 +2,17 @@ package pl.mm.sportmetrics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SportmetricsApplication {
+public class SportmetricsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder builder) {
+        return builder.sources(SportmetricsApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SportmetricsApplication.class, args);
     }
